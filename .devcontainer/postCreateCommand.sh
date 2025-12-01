d80dc614b1b07053c495633e61e793b1489e57d4
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-11-30 22:53:37 -0500

    chore(devcontainer): add secrets pulling post create

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+pip3 install -r scripts/requirements.txt
+
+# Install xdg-utils for Vault login
+sudo apt-get update -y
+sudo apt-get install -y --no-install-recommends xdg-utils
+sudo rm -rf /var/lib/apt/lists/*
+
+# Pull secrets from Vault
+./scripts/secrets-setup.sh
+./scripts/secrets-pull.sh
