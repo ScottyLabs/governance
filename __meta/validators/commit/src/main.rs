0f3d82f510751ced9c8ff391f3675f51c1c7f4e7
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-12-27 23:37:40 -0500

    docs: add conventional commit link when user fail check

--- __meta/validators/commit/src/main.rs
+++ __meta/validators/commit/src/main.rs
@@ -25,6 +25,7 @@ fn main() {
             eprintln!("Examples:");
             eprintln!("  feat: add user authentication");
             eprintln!("  fix(api): handle edge case in login flow");
+            eprintln!("See https://www.conventionalcommits.org/en/v1.0.0/ for more information.");
             process::exit(1);
         }
     }
