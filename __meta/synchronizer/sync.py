7232727a422d220f07c9cb5539822d5cb1f57fe1
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-12-29 22:48:26 -0500

    feat(sync): add arg parser for main script

--- __meta/synchronizer/sync.py
+++ __meta/synchronizer/sync.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 import tomllib
@@ -39,11 +40,57 @@ class SyncManager:
                     self.teams[team_name] = tomllib.loads(f.read())
 
     def sync(self):
+        self.sync_github()
+        self.sync_keycloak()
+        self.sync_vault()
+        self.sync_slack()
+
+    def sync_github(self):
         GithubManager(self.contributors, self.teams).sync()
+
+    def sync_keycloak(self):
         KeycloakManager(self.contributors, self.teams).sync()
+
+    def sync_vault(self):
         VaultManager(self.teams).sync()
+
+    def sync_slack(self):
         SlackManager(self.contributors, self.teams).sync()
 
 
+def args_parser():
+    parser = argparse.ArgumentParser(
+        description="Sync the teams and members from the contributors and teams directories to ScottyLabs services."
+    )
+    parser.add_argument(
+        "--services",
+        nargs="+",
+        choices=["github", "keycloak", "vault", "slack"],
+        default=["github", "keycloak", "vault", "slack"],
+        metavar="SERVICE",
+        help=(
+            "One or more services to sync "
+            "(choices: github, keycloak, vault, slack). "
+            "Defaults to syncing all."
+        ),
+    )
+    return parser
+
+
 if __name__ == "__main__":
-    SyncManager().sync()
+    # Parse the arguments
+    parser = args_parser()
+    args = parser.parse_args()
+
+    # Initialize the sync manager
+    sync_manager = SyncManager()
+    service_name_to_function = {
+        "github": sync_manager.sync_github,
+        "keycloak": sync_manager.sync_keycloak,
+        "vault": sync_manager.sync_vault,
+        "slack": sync_manager.sync_slack,
+    }
+
+    # Sync the services
+    for service_name in args.services:
+        service_name_to_function[service_name]()
