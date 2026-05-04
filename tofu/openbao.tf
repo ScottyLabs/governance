d8d6e8a33749243e80993c831a85366345f37ecd
Anish Pallati <i@anish.land>
2026-05-03 21:39:48 -0400

    feat: add cachix-pull policy for project members

--- tofu/openbao.tf
+++ tofu/openbao.tf
@@ -44,3 +44,24 @@ resource "vault_identity_group_alias" "devops" {
     mount_accessor = data.vault_auth_backend.oidc.accessor
     canonical_id   = vault_identity_group.devops.id
 }
+
+resource "vault_policy" "cachix_pull" {
+    name   = "cachix-pull"
+    policy = <<-EOT
+        path "secret/data/shared/cachix" {
+            capabilities = ["read"]
+        }
+    EOT
+}
+
+resource "vault_identity_group" "all_members" {
+    name     = "all-members"
+    type     = "external"
+    policies = [vault_policy.cachix_pull.name]
+}
+
+resource "vault_identity_group_alias" "all_members" {
+    name           = "/projects"
+    mount_accessor = data.vault_auth_backend.oidc.accessor
+    canonical_id   = vault_identity_group.all_members.id
+}
