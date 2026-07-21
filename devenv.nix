502e842422fd68c5bcab445ed49c057288a07e91
Anish Pallati <i@anish.land>
2026-07-20 22:31:49 -0400

    feat: adopt devenv in governance
    Signed-off-by: Anish Pallati <i@anish.land>

---
+++ devenv.nix
@@ -0,0 +1,11 @@
+{ inputs, ... }:
+
+{
+  imports = [ inputs.scottylabs.devenvModules.default ];
+
+  scottylabs = {
+    enable = true;
+    project.name = "governance";
+    rust.enable = true;
+  };
+}
